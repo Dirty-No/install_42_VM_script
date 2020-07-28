4d624ac61dc65d126c9a24135880d1b744e2318a
smaccary <smaccary@student.42.fr>
2020-07-28 23:30:27 +0200

    added ext pack dl

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-sudo apt-get install -yqq  virtualbox virtualbox-dkms virtualbox-qt
+sudo apt-get install -yqq  virtualbox virtualbox-ext-pack virtualbox-dkms virtualbox-qt
 wget https://cdn.42.fr/xubuntu-42.ova
 virtualbox xubuntu-42.ova
